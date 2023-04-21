a7386817958e42b759afe55761ef726c1456abd2
thjalfe <thjalfe96@gmail.com>
2023-04-21 12:26:59 +0200

    initial commit

---
+++ IM-FWM/delta_wl_sweep/test_script.py
@@ -0,0 +1,84 @@
+import numpy as np
+from util_funcs import (
+    analyze_data,
+    load_raw_data,
+    get_all_unique_pairs_list,
+    load_pump_files,
+)
+from plotting_funcs import save_plot, plot_top_n_datasets
+import matplotlib.pyplot as plt
+import os
+
+plt.style.use("custom")
+plt.ion()
+
+data_folder = "./data_pulsed/"
+# data_folder = data_folder[:-1]
+file_type = "pkl"
+unique_pairs = get_all_unique_pairs_list(data_folder, file_type=file_type)
+# unique_pairs = (1569.0, 1573.0)
+sorted_peak_data = analyze_data(
+    data_folder, pump_wl_pairs=unique_pairs, file_type=file_type
+)
+data, keys, other = load_raw_data(
+    data_folder, -85, None, unique_pairs, file_type=file_type
+)
+# |%%--%%| <bUxbvLaNgk|58VOLX1DCi>
+# Plot the top n datasets
+n = 5  # Top 5 datasets
+idx = 2
+plot_top_n_datasets(
+    sorted_peak_data[unique_pairs[idx]], data[idx], n, unique_pairs[idx]
+)
+save_plot(
+    f"./figs/pulsed/CE_top_{n}_datasets_{unique_pairs[idx][0]}_{unique_pairs[idx][1]}_weird_peak"
+)
+# |%%--%%| <58VOLX1DCi|79QO9wN6Wo>
+best_ce = []
+best_ce_loc = []
+best_idx = []
+for i, key in enumerate(keys):
+    best_idx.append(list(sorted_peak_data[key].keys())[0])
+    best_ce.append(min(sorted_peak_data[key][best_idx[i]]["differences"]))
+    temp_best_loc = np.argmin(sorted_peak_data[key][best_idx[i]]["differences"])
+    best_ce_loc.append(
+        sorted_peak_data[key][best_idx[i]]["peak_positions"][temp_best_loc]
+    )
+    # best_ce.append(sorted_peak_data[key][0])
+    # best_ce_loc.append(sorted_peak_data[key][1])
+x = np.arange(2, len(best_ce) + 2)
+fig, ax = plt.subplots(2, 1)
+ax[0].plot(x, -np.array(best_ce), marker="o", linestyle="-")
+ax[0].set_ylabel("CE [dB]")
+ax[0].grid(True)
+ax[1].plot(x, best_ce_loc, marker="o", linestyle="-")
+ax[1].set_xlabel("Pump separation [nm]")
+ax[1].set_ylabel("Signal location [nm]")
+ax[1].grid(True)
+save_plot("./figs/pulsed/CE_vs_pump_separation")
+# |%%--%%| <79QO9wN6Wo|Ftmiqvm323>
+from scipy.signal import find_peaks
+
+
+char_folder = "./char_setup/"
+file_str = "pump_sep_through_setup_"
+file_str = "pumps_directly_to_osa"
+file_str = "pump_directly_to_osa"
+# file_str = 'before_edfa'
+pumps, unique_pump_pairs = load_pump_files(char_folder, pump_name=file_str)
+
+fig, ax = plt.subplots()
+for i in range(pumps.shape[0]):
+    peaks, _ = find_peaks(pumps[i, :, 1], height=-80)
+    peaks_max = peaks[np.argsort(pumps[i, peaks, 1])[-2:]]
+    ax.plot(
+        pumps[i, :, 0],
+        pumps[i, :, 1],
+        label=f"{unique_pump_pairs[i]} nm, power diff: {pumps[i, peaks_max[0], 1] - pumps[i, peaks_max[1], 1]:.2f} dB",
+    )
+ax.set_xlabel("Wavelength [nm]")
+ax.set_ylabel("Power [dBm]")
+ax.grid(True)
+# ax.legend(loc="lower right")
+# plt.show()
+save_plot(f"./figs/char_setup/{file_str}")
---
+++ IM-FWM/delta_wl_sweep/util_funcs.py
@@ -0,0 +1,288 @@
+import numpy as np
+import scipy.signal
+import re
+import os
+import glob
+import pickle
+from natsort import natsorted
+
+
+def load_files(file_paths):
+    loaded_files = []
+    extra_data_list = []
+    if file_paths[0].endswith(".pkl"):
+        for file_path in file_paths:
+            with open(file_path, "rb") as f:
+                loaded_data = pickle.load(f)
+                # Ensure 'wavelengths' and 'powers' are the keys in the dictionary
+                assert "wavelengths" in loaded_data and "powers" in loaded_data
+                # Combine the wavelengths and powers into a 2D numpy array
+                combined_data = np.column_stack(
+                    (loaded_data["wavelengths"], loaded_data["powers"])
+                )
+                loaded_files.append(combined_data)
+
+                # Remove 'wavelengths' and 'powers' from the loaded_data dictionary
+                del loaded_data["wavelengths"]
+                del loaded_data["powers"]
+                extra_data_list.append(loaded_data)
+    else:
+        for file_path in file_paths:
+            loaded_data = np.loadtxt(file_path, delimiter=",")
+            loaded_files.append(loaded_data)
+        extra_data_list = None
+
+    loaded_files = np.stack(np.atleast_1d(loaded_files))
+    return loaded_files, extra_data_list
+
+
+def filter_files_by_pump_wl_pair(file_list, chosen_pair, pattern):
+    file_paths = [
+        file_name
+        for file_name in file_list
+        if pattern.match(file_name)
+        and (
+            float(pattern.match(file_name).group(1)),
+            float(pattern.match(file_name).group(2)),
+        )
+        == chosen_pair
+    ]
+    return file_paths
+
+
+def possible_idler_locations(sig_wl, p1_wl, p2_wl):
+    """
+    Returns a list of possible idler wavelengths given pump wavelengths.
+
+    Args:
+        sig_wl (float): Signal wavelength.
+        p1_wl (float): First pump wavelength.
+        p2_wl (float): Second pump wavelength.
+
+    Returns:
+        list: A list of possible idler wavelengths.
+    """
+    return [
+        1 / (1 / sig_wl + 1 / p1_wl - 1 / p2_wl),
+        1 / (1 / sig_wl - 1 / p1_wl + 1 / p2_wl),
+    ]
+
+
+def return_filtered_peaks(
+    data, prominence, height, pump_wavelengths, tolerance_nm, max_peak_height
+):
+    """
+    Filters the peaks based on prominence, height, pump wavelengths and tolerance_nm.
+
+    Args:
+        data (numpy.ndarray): 2D array containing x and y values of the dataset.
+        prominence (float): Minimum prominence of the peaks.
+        height (float): Minimum height of the peaks.
+        pump_wavelengths (tuple): Tuple containing two pump wavelengths.
+        tolerance_nm (float): Tolerance for peak position deviation.
+
+    Returns:
+        list: A list of filtered peaks.
+    """
+    wl_res = data[1, 0] - data[0, 0]
+    tolerance_idxs = np.ceil(tolerance_nm / wl_res)
+    peaks, _ = scipy.signal.find_peaks(data[:, 1], prominence=prominence, height=height)
+    # Find the maximum peak location
+    max_peak_loc = peaks[np.nanargmax(data[peaks, 1])]
+    if data[max_peak_loc, 1] < max_peak_height:
+        return ["No peak found"]
+    # Calculate peak_positions based on energy_requirements
+    peak_positions = possible_idler_locations(
+        data[max_peak_loc, 0], pump_wavelengths[0], pump_wavelengths[1]
+    )
+    peak_position_indices = [
+        np.nanargmin(np.abs(data[:, 0] - peak_position))
+        for peak_position in peak_positions
+    ]
+    filtered_peaks = [max_peak_loc]
+    for pos in peak_position_indices:
+        peak = min(peaks, key=lambda x: abs(x - pos))
+        if abs(peak - pos) <= tolerance_idxs:
+            filtered_peaks.append(peak)
+    return filtered_peaks
+
+
+def calculate_differences(peaks, data):
+    """
+    Calculate the differences between the highest peak and other peaks.
+
+    Args:
+        peaks (list): List of peak indices.
+        data (numpy.ndarray): 2D array containing x and y values of the dataset.
+
+    Returns:
+        list: A list of differences between the highest peak and other peaks.
+    """
+    peak_values = [data[p, 1] for p in peaks]
+
+    # Find the index of the highest peak
+    highest_peak_index = np.argmax(peak_values)
+    highest_peak_value = peak_values[highest_peak_index]
+
+    peak_differences = [
+        abs(highest_peak_value - peak_values[i])
+        for i in range(len(peak_values))
+        if i != highest_peak_index
+    ]
+    return peak_differences
+
+
+def process_dataset(
+    dataset, prominence, height, peak_positions, tolerance_nm, max_peak_height
+):
+    """
+    Process the dataset to find peaks, differences, peak values, and peak positions.
+
+    Args:
+        dataset (numpy.ndarray): 3D array containing data for each file.
+        prominence (float): Minimum prominence of the peaks.
+        height (float): Minimum height of the peaks.
+        peak_positions (tuple): Tuple containing two pump wavelengths.
+        tolerance_nm (float): Tolerance_nm for peak position deviation.
+        max_peak_height (float, optional): Maximum peak height. This is to avoid false good measurements, in case a measurement failed and did not detect any peak at all.
+
+    Returns:
+        dict: A dictionary containing peaks, differences, peak values, and peak positions for each file.
+    """
+    peak_data = {}
+    for m in range(dataset.shape[0]):
+        data = dataset[m]
+        peaks = return_filtered_peaks(
+            data, prominence, height, peak_positions, tolerance_nm, max_peak_height
+        )
+        if peaks == ["No peak found"]:
+            peak_data[m] = "No peak found"
+            continue
+        if len(peaks) == 1:
+            peak_data[m] = "Only signal peak found"
+            continue
+        differences = calculate_differences(peaks, data)
+        peak_data[m] = {
+            "peaks": peaks,
+            "differences": differences,
+            "peak_values": [data[p, 1] for p in peaks],
+            "peak_positions": [data[p, 0] for p in peaks],
+        }
+    return peak_data
+
+
+def sort_peak_data(peak_data, sort_key):
+    return dict(
+        sorted(
+            peak_data.items(),
+            key=lambda x: min(x[1][sort_key]) if type(x[1]) == dict else float("inf"),
+        )
+    )
+
+
+def load_raw_data(
+    data_folder, nan_filter, pair_indices=None, pump_wl_pairs=None, file_type="pkl"
+):
+    data_folder = data_folder[:-1]
+    file_list = natsorted(glob.glob(data_folder + f"/*[0-9].{file_type}"))
+    pattern = re.compile(rf"{data_folder}[\\/]([\d.]+)_([\d.]+)_\d+.{file_type}")
+    if pair_indices is not None:
+        unique_pairs = set()
+        for file_name in file_list:
+            match = pattern.match(file_name)
+            if match:
+                pair = (float(match.group(1)), float(match.group(2)))
+                unique_pairs.add(pair)
+        unique_pairs_list = list(unique_pairs)[::-1]
+        chosen_pairs = [unique_pairs_list[pair_index] for pair_index in pair_indices]
+    elif pump_wl_pairs is not None:
+        chosen_pairs = pump_wl_pairs
+    else:
+        raise TypeError(
+            'You must choose to pick files either with "pair_indices" or "pump_wl_pairs"'
+        )
+    # If only one pair is chosen, convert it to a list such that it can be iterated over
+    if type(chosen_pairs) == tuple:
+        chosen_pairs = [chosen_pairs]
+    filtered_file_lists = []
+    for pair in chosen_pairs:
+        filtered_file_list = filter_files_by_pump_wl_pair(file_list, pair, pattern)
+        filtered_file_lists.append(filtered_file_list)
+
+    data = []
+    extra_data = []
+    for filtered_file_list in filtered_file_lists:
+        dataset, extra_dataset = load_files(filtered_file_list)
+        data.append(dataset)
+        extra_data.append(extra_dataset)
+
+    for dataset in data:
+        dataset[dataset[:, :, 1] < nan_filter, 1] = np.nan
+
+    return data, chosen_pairs, extra_data
+
+
+def analyze_data(
+    data_folder,
+    pair_indices=None,
+    pump_wl_pairs=None,
+    nan_filter=-85,
+    prominence=0.1,
+    height=-65,
+    tolerance_nm=0.25,
+    max_peak_height=-25,
+    file_type="pkl",
+):
+    data, chosen_pairs, _ = load_raw_data(
+        data_folder, nan_filter, pair_indices, pump_wl_pairs, file_type
+    )
+
+    all_sorted_peak_data = {}
+    for i, dataset in enumerate(data):
+        peak_data = process_dataset(
+            dataset, prominence, height, chosen_pairs[i], tolerance_nm, max_peak_height
+        )
+        sorted_peak_data = sort_peak_data(peak_data, "differences")
+        all_sorted_peak_data[chosen_pairs[i]] = sorted_peak_data
+
+    return all_sorted_peak_data
+
+
+def get_all_unique_pairs_list(data_folder, file_type="pkl"):
+    data_folder = data_folder[:-1]
+    file_list = natsorted(glob.glob(data_folder + f"/*[0-9].{file_type}"))
+    pattern = re.compile(rf"{data_folder}[\\/]([\d.]+)_([\d.]+)_\d+.{file_type}")
+    unique_pairs = set()
+    for file_name in file_list:
+        match = pattern.match(file_name)
+        if match:
+            pair = (float(match.group(1)), float(match.group(2)))
+            unique_pairs.add(pair)
+    unique_pairs_list = list(unique_pairs)
+    sorted_unique_pairs_list = sorted(unique_pairs_list, key=lambda x: x[0])
+    return sorted_unique_pairs_list[::-1]
+
+
+def extract_pump_numbers(file_path, pump_name):
+    match = re.search(rf"{pump_name}(\d+\.\d+)_(\d+\.\d+)_", file_path)
+    if match:
+        return float(match.group(1)), float(match.group(2))
+    else:
+        return 0, 0
+
+
+def load_pump_files(data_folder, pump_name="pumps", nan_filter=-80):
+    pattern = os.path.join(data_folder, f"{pump_name}*")
+    matching_files = glob.glob(pattern)
+    # Sort the matching_files list based on the first number in descending order
+    sorted_files = sorted(
+        matching_files, key=lambda x: extract_pump_numbers(x, pump_name), reverse=True
+    )
+    pumps = []
+    number_tuples = []
+    for file in sorted_files:
+        pumps.append(np.loadtxt(file, delimiter=","))
+        number_tuples.append(extract_pump_numbers(file, pump_name))
+    pumps = np.array(pumps)
+    pumps[:, :, 1][pumps[:, :, 1] < nan_filter] = np.nan
+    return pumps, number_tuples
